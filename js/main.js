ee74151ed9a6c63cbf775a03f2d06b25f1fd613b
Diego Azevedo <dapptn@hotmail.com>
2019-06-26 19:59:50 +1000

    Added imc for all patients

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
 var title = document.querySelector(".title");
 title.textContent = "Luke's Nutrition";
 
+function calculateIMC(height, weight) {
+    return weight / (height * height);
+}
+
+let patientsTr = document.getElementsByClassName('patient');
+for(let i =0; i < patientsTr.length; i++) {
+    
+    let height = patientsTr[i].querySelector('.info-altura').textContent;
+    let weight = patientsTr[i].querySelector('.info-peso').textContent;
+
+    let imc = calculateIMC(height, weight);
+    patientsTr[i].querySelector('.info-imc').textContent = imc.toFixed(2);
+}
