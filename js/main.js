a8b3990a22e934c69d7e1e7d430301b3b8917ec3
Diego Azevedo <dapptn@hotmail.com>
2019-06-27 16:05:37 +1000

    Added verification for for weight and height

--- js/main.js
+++ js/main.js
@@ -5,12 +5,44 @@ function calculateIMC(height, weight) {
     return weight / (height * height);
 }
 
+function checkHeight(height) {
+    return ( height > 0 && height < 3 );
+}
+
+function checkWeight(weight) {
+    return ( weight >= 0 && weight < 1000 );
+}
+
 let patientsTr = document.getElementsByClassName('patient');
+
 for(let i =0; i < patientsTr.length; i++) {
     
-    let height = patientsTr[i].querySelector('.info-altura').textContent;
-    let weight = patientsTr[i].querySelector('.info-peso').textContent;
+    let error = false;
 
-    let imc = calculateIMC(height, weight);
-    patientsTr[i].querySelector('.info-imc').textContent = imc.toFixed(2);
+    let $heightTd = patientsTr[i].querySelector('.info-altura');
+    let $weightTd = patientsTr[i].querySelector('.info-peso');
+    let $imcTd = patientsTr[i].querySelector('.info-imc');
+
+    let weight = $weightTd.textContent;
+    let height = $heightTd.textContent;
+    
+    if(checkWeight(weight) == false) {
+        error = true;
+        console.log('Invalid Weight');
+        $weightTd.textContent = 'Invalid weight';
+    }
+
+    if(checkHeight(height) == false) {
+        error = true;
+        console.log('Invalid height');
+        $heightTd.textContent = 'Invalid height';
+    }
+        
+    if(!error) {
+        let imc = calculateIMC(height, weight);
+        $imcTd.textContent = imc.toFixed(2);
+    } else {
+        $imcTd.textContent = 'Invalid data input';
+    }
 }
+
