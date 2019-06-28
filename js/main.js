6b854671e6a9ed09b13db68090b76fb621995c7d
Diego Azevedo <dapptn@hotmail.com>
2019-06-28 15:00:31 +1000

    Implented the form to add patients

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
+'use strict';
+
 var title = document.querySelector(".title");
 title.textContent = "Luke's Nutrition";
 
 function calculateIMC(height, weight) {
+    console.log(arguments)
     return weight / (height * height);
 }
 
@@ -42,6 +45,48 @@ for(let i =0; i < $patients.length; i++) {
         $patient.querySelector('.info-imc').textContent = 'Invalid data';
         $patient.classList.add('invalid-pacient');
     }
-
 }
+
+var $addButton = document.querySelector('#adicionar-paciente');
+$addButton.addEventListener('click', function(event) {
+    
+    event.preventDefault();
+    
+    let $form = document.querySelector('#form-adiciona');
+    let patientName = $form.nome.value;
+    let patientHeight = $form.altura.value;
+    let patientWeight = $form.peso.value;
+    let patientFat = $form.gordura.value;
+    
+    let newRow = document.createElement('tr');
+    newRow.classList.add('patient');
+    
+    var tdName = document.createElement('td');
+    tdName.classList.add('info-nome');
+    tdName.textContent = patientName;
+    newRow.appendChild(tdName);
+   
+    let tdWeigth = document.createElement('td');
+    tdWeigth.classList.add('info-peso');
+    tdWeigth.textContent = patientWeight;
+    newRow.appendChild(tdWeigth);
+
+    let tdHeigth = document.createElement('td');
+    tdHeigth.classList.add('info-altura');
+    tdHeigth.textContent = patientHeight;
+    newRow.appendChild(tdHeigth);
+
+    let tdFat = document.createElement('td');
+    tdFat.classList.add('info-gordura');
+    tdFat.textContent = patientFat;
+    newRow.appendChild(tdFat);
+
+    let tdImc = document.createElement('td');
+    tdImc.classList.add('info-imc');
+    tdImc.textContent = calculateIMC(patientHeight, patientWeight).toFixed(2);
+    newRow.appendChild(tdImc);
+
+    document.querySelector('#table-patients').appendChild(newRow);
+    
+})
 
