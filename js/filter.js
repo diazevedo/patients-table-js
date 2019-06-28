a0d7a8bd62e1c2c8a222fc212d9073de4ca429de
Diego Azevedo <dapptn@hotmail.com>
2019-06-28 23:03:38 +1000

    Field to search patients by  name

---
+++ js/filter.js
@@ -0,0 +1,23 @@
+var $inputSearch = document.querySelector('#filter');
+
+$inputSearch.addEventListener("input", function () {
+    var $patients = document.querySelectorAll(".patient");
+    
+    if(this.value.length > 0) {
+        
+        var regExpression = new RegExp(this.value, 'i'); /*case insensitive*/
+
+        $patients.forEach( $patient => {
+            let name = $patient.querySelector('.info-nome').textContent;
+            
+            if(!regExpression.test(name))
+                $patient.classList.add('hidden-tr');
+            else
+                $patient.classList.remove('hidden-tr');
+
+        });
+
+    } else {
+        $patients.forEach( $patient => $patient.classList.remove('hidden-tr'));
+    } 
+});
